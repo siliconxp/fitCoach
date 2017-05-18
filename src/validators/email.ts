8b7d58ab47536a08c19c9007a115f93dd698567f
Jorge Vergara <javebratt@gmail.com>
2017-05-18 05:31:08 -0500

    Update email validator

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -3,8 +3,7 @@ import {FormControl} from '@angular/forms';
 export class EmailValidator {
 
     static isValid(control: FormControl){
-
-      var re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(control.value);
+      const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(control.value);
 
       if (re){
         return null;
